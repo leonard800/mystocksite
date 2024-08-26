c9eac33cdb9525ecc33dd5a6c5489a947b770b96
leonard800 <leonardabimanyu@gmail.com>
2024-08-26 16:50:43 +0700

    first init

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+
+const Header = () => {
+
+    return(
+        <div></div>
+    )
+}
